badb9656ea4af908f472a6b93055de868ccdb9cc
Sacha Telgenhof <me@sachatelgenhof.com>
2020-09-10 09:05:37 +0900

    First setup introducing PHPSpec.
    Signed-off-by: Sacha Telgenhof <me@sachatelgenhof.com>

---
+++ spec/Yasumi/Provider/JapanSpec.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 - 2020 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me@sachatelgenhof.com>
+ */
+
+namespace spec\Yasumi\Provider;
+
+use PhpSpec\ObjectBehavior;
+use Yasumi\Provider\AbstractProvider;
+use Yasumi\Provider\Japan;
+
+class JapanSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith(2020);
+    }
+
+    public function it_is_initializable(): void
+    {
+        $this->shouldHaveType(Japan::class);
+    }
+
+    public function it_extends_abstract_provider(): void
+    {
+        $this->shouldBeAnInstanceOf(AbstractProvider::class);
+    }
+}
---
+++ spec/Yasumi/Provider/NetherlandsSpec.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the Yasumi package.
+ *
+ * Copyright (c) 2015 - 2020 AzuyaLabs
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Sacha Telgenhof <me@sachatelgenhof.com>
+ */
+
+namespace spec\Yasumi\Provider;
+
+use PhpSpec\ObjectBehavior;
+use Yasumi\Provider\AbstractProvider;
+use Yasumi\Provider\Netherlands;
+
+class NetherlandsSpec extends ObjectBehavior
+{
+    public function let(): void
+    {
+        $this->beConstructedWith(2020);
+    }
+
+    public function it_is_initializable(): void
+    {
+        $this->shouldHaveType(Netherlands::class);
+    }
+
+    public function it_extends_abstract_provider(): void
+    {
+        $this->shouldBeAnInstanceOf(AbstractProvider::class);
+    }
+
+    public function it_should_have_new_years_day(): void
+    {
+        $this->isHoliday(new \DateTimeImmutable('2020-01-01'))->shouldBe(true);
+    }
+}
